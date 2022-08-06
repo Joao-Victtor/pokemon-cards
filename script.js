2099ba349666539db8f61939bf9d7d8da4dec132
João Victor <joaoviccttor.dev@proton.me>
2022-08-06 12:26:09 -0400

    add javascript

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+const btnAvancar = document.getElementById('btn-avancar') 
+const btnVoltar = document.getElementById('btn-voltar')
+const cartoes = document.querySelectorAll('.cartao')
+let cartaoAtual = 0
 
+function esconderCartao() {
+    const cartaoSelecionado = document.querySelector(".visivel")
+    cartaoSelecionado.classList.remove("visivel")
+}
+
+function mostrarCartao(indiceCartao) {
+    cartoes[indiceCartao].classList.add("visivel")
+}
+
+btnAvancar.addEventListener("click", function() {
+    if (cartaoAtual === cartoes.length - 1) return;
+    
+    esconderCartao()
+    
+    cartaoAtual++
+    mostrarCartao(cartaoAtual)
+})
+
+btnVoltar.addEventListener("click", function () {
+    if (cartaoAtual === 0) return;
+
+    esconderCartao()
+    
+    cartaoAtual--
+    mostrarCartao(cartaoAtual)
+})
